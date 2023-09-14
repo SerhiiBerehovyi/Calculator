c65616d5c39c5998a1a8b9a47f4289d99f166e60
Serhii Berehovyi <serhii.berehovyi@gmail.com>
2023-09-14 16:29:50 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Serhii Berehovyi on 14.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
